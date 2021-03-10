4650b3feff02267720f4355d8a182010f3b7efee
ronaksinghbagga <ronak1851996@gmail.com>
2021-03-10 16:48:03 +0530

    first commit

---
+++ src/app/pages/main-view/main-view.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import {CdkDragDrop, moveItemInArray, transferArrayItem} from '@angular/cdk/drag-drop';
+import { Board } from 'src/app/models/board.model';
+import { Column } from 'src/app/models/column.model';
+
+@Component({
+  selector: 'app-main-view',
+  templateUrl: './main-view.component.html',
+  styleUrls: ['./main-view.component.scss']
+})
+export class MainViewComponent implements OnInit {
+
+  constructor() { }
+
+  board: Board = new Board('Test Board', [
+    new Column('Ideas', ['abc', 'def', 'ghi']),
+  new Column('Reasearch', ['jkl', 'mno']),
+new Column('Done', ['pqr', 'stu', 'vwx', 'yz'])]);
+
+  ngOnInit(): void {
+  }
+  drop(event: CdkDragDrop<string[]>): void {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(event.previousContainer.data,
+                        event.container.data,
+                        event.previousIndex,
+                        event.currentIndex);
+    }
+  }
+}
